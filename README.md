867b0285713acb998ec7d5441d4e590010aa4628
Martín Gaitán <gaitan@gmail.com>
2023-01-18 15:51:20 -0300

    refactor. move to own account

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # hay-camisetas-bot
 ¿Hay camisetas ⭐⭐⭐?
 
-Este bot se ejecuta cada 20 minutos, se fija en el sitio de Adidas si hay stock de la camiseta y si encuentra lo tuitea, por ahora en 
-[@tin_nqn_](https://twitter.com/tin_nqn_). 
+Este bot se ejecuta cada 15 minutos, se fija en el sitio de Adidas si hay stock de la camiseta y si encuentra lo tuitea, 
+[@haycamisetas](https://twitter.com/haycamisetas). 
 
 
 https://www.adidas.com.ar/camiseta-titular-argentina-3-estrellas-2022/IB3593.html
--- camisetas_bot.py
+++ camisetas_bot.py
@@ -1,18 +1,37 @@
 import os
+import random
 import sys
 
 import requests
 import tweepy
 
-headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+}
 
-data = requests.get("https://www.adidas.com.ar/api/products/IB3593/availability", headers=headers).json()
-if not data["availability_status"] == "IN_STOCK":
-    print("no hay stock")
-    sys.exit(1)
-available = [a["size"] for a in data["variation_list"] if a["availability"]]
-msg = "Hay stock de camisetas ⭐⭐⭐ del campeón del Mundo. "
-msg += f"Talles {', '.join(i for i in available[:-1])} y {available[-1]}." if len(available) > 1 else f"Talle {available[0]} únicamente."
+data = requests.get(
+    "https://www.adidas.com.ar/api/products/IB3593/availability", headers=headers
+).json()
+NO_STOCK_MESSAGES = [
+    "Ya no quedan 😔. Pero te aviso en cuanto me entere.",
+    "Se acabaron. Vuelan. Tss tss tss.",
+    "Durmieron, pero tranqui que me ocupo.",
+    "Fue bueno mientras duró. Pero seguimos siendo campeones del mundo.",
+]
+
+
+if data["availability_status"] == "IN_STOCK":
+    available = [a["size"] for a in data["variation_list"] if a["availability"]]
+    msg = "Hay stock de camisetas ⭐⭐⭐ del campeón del Mundo. "
+    msg += (
+        f"Talles {', '.join(i for i in available[:-1])} y {available[-1]}."
+        if len(available) > 1
+        else f"Talle {available[0]} únicamente."
+    )
+else:
+    msg = random.choice(NO_STOCK_MESSAGES)
+
+print(msg)
 
 
 if len(sys.argv) > 1 and sys.argv[1] == "--tweet":
@@ -22,9 +41,20 @@ if len(sys.argv) > 1 and sys.argv[1] == "--tweet":
     access_token_secret = os.environ["TW_ACCESS_TOKEN_SECRET"]
 
     client = tweepy.Client(
-        consumer_key=consumer_key, consumer_secret=consumer_secret,
-        access_token=access_token, access_token_secret=access_token_secret
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
     )
+    me = client.get_me()
+    last_tweet = client.get_users_tweets(
+        me.data.id, user_auth=True, max_results=5
+    ).data[0]
 
-    response = client.create_tweet(text=f"{msg} https://www.adidas.com.ar/camiseta-titular-argentina-3-estrellas-2022/IB3593.html")
-    print(f"https://twitter.com/user/status/{response.data['id']}")
+    # only tweet one "no stock" message
+    if str(last_tweet) not in NO_STOCK_MESSAGES:
+
+        response = client.create_tweet(
+            text=f"{msg} https://www.adidas.com.ar/camiseta-titular-argentina-3-estrellas-2022/IB3593.html"
+        )
+        print(f"https://twitter.com/user/status/{response.data['id']}")
